74f59d791caa5816aa706da914d94399b78fc37d
Senen Fernandez <senen@senenfernandez.com>
2016-07-02 11:43:48 -0500

    SELECT Prime Numbers

---
+++ SQLServer/PrimeNumbers.sql
@@ -0,0 +1,21 @@
+﻿DECLARE @maxPrime INT = 1000
+DECLARE @prime INT = 1
+DECLARE @isPrime BIT
+DECLARE @divisor INT
+DECLARE @primeTable TABLE(PrimeNumber INT)
+WHILE @prime <= @maxPrime
+BEGIN
+	SET @isPrime = 1
+	SET @divisor = 2
+	WHILE @divisor < @prime AND @isPrime != 0
+	BEGIN
+		SET @isPrime = @prime % @divisor
+		SET @divisor = @divisor + 1
+	END
+	IF @isPrime != 0
+	BEGIN
+		INSERT INTO @primeTable VALUES(@prime)
+	END
+	SET @prime = @prime + 1
+END
+SELECT PrimeNumber FROM @primeTable
